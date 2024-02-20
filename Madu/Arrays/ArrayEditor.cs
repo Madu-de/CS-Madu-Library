c1d125c9e5ebd28de74fb8ce8c17ed60d4b9fffa
Madu <kontakt.madu@gmail.com>
2024-02-20 21:50:53 +0100

    Initial commit

---
+++ Madu/Arrays/ArrayEditor.cs
@@ -0,0 +1,94 @@
+﻿/**
+* ArrayEditor
+* Copyright © Madu/Marvin - 2024
+*/
+namespace Madu.Arrays
+{
+    /// <summary>
+    /// A little editor to remove elements of an array
+    /// </summary>
+    public class ArrayEditor
+    {
+        /// <summary>
+        /// Removes the first element which has the same value as element
+        /// </summary>
+        /// <returns>The modified array</returns>
+        public T[] RemoveElement<T>(T[] arr, T element)
+        {
+            int i = FindFirstIndexOf(arr, element);
+            return RemoveElementByIndex(arr, i);
+        }
+
+        /// <summary>
+        /// Removes all elements which have the same value as element
+        /// </summary>
+        /// <returns>The modified array</returns>
+        public T[] RemoveElements<T>(T[] arr, T element)
+        {
+            while (IsElementInArray(arr, element))
+            {
+                arr = RemoveElement(arr, element);
+            }
+            return arr;
+        }
+
+        /// <summary>
+        /// Removes the element at index i
+        /// </summary>
+        /// <exception cref="IndexOutOfRangeException">
+        /// Thrown when the index is out of the array range
+        /// </exception>
+        /// <returns>The modified array</returns>
+        public T[] RemoveElementByIndex<T>(T[] arr, int i)
+        {
+            if (i == -1) return arr;
+            return MoveElementsUpTo(arr, i);
+        }
+
+        /// <summary>
+        /// Returns -1 if element isn't in array
+        /// </summary>
+        /// <returns>Index of the element or -1</returns>
+        int FindFirstIndexOf<T>(T[] arr, T element)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]!.Equals(element))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns true if element is in array
+        /// </summary>
+        bool IsElementInArray<T>(T[] arr, T element)
+        {
+            return FindFirstIndexOf(arr, element) != -1;
+        }
+
+        /// <summary>
+        /// Moves all right hand elements one index to the left
+        /// </summary>
+        /// <exception cref="IndexOutOfRangeException">
+        /// Thrown when the index is out of the array range
+        /// </exception>
+        /// <returns>The modified element</returns>
+        T[] MoveElementsUpTo<T>(T[] arr, int index)
+        {
+            int newArrayLength = arr.Length - 1;
+            if (index > newArrayLength || index < 0)
+            {
+                throw new IndexOutOfRangeException($"The index has to be in the array range (0 - {newArrayLength}) Is: {index}");
+            }
+            T[] outputArray = new T[newArrayLength];
+            for (int i = 0; i < newArrayLength; i++)
+            {
+                outputArray[i] = i >= index ? arr[i + 1] : arr[i];
+            }
+            return outputArray;
+        }
+    }
+}
---
+++ Madu/Util/UserInputHandler.cs
@@ -0,0 +1,86 @@
+﻿/**
+* GetUserInput
+* Copyright © Madu/Marvin - 2024
+*/
+namespace Madu.Util
+{
+    public class UserInputHandler
+    {
+        private string GetInput(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Returns the default value of T if the userinput is not compatible
+        /// </summary>
+        public T? GetInputAs<T>(string question)
+        {
+            try
+            {
+                return GetInputAsWithoutCheck<T>(question);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// Throws an error if the userinput is not compatible
+        /// </summary>
+        public T GetInputAsWithoutCheck<T>(string question)
+        {
+            return (T)Convert.ChangeType(GetInput(question), typeof(T));
+        }
+
+        /// <summary>
+        /// Creates a selection list
+        /// </summary>
+        /// <returns>
+        /// The index of the selected list element
+        /// </returns>
+        public int GetInputWithList(string message, string[] list)
+        {
+            int choise = 0;
+            bool runloop = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                PrintList(list, choise);
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                switch (key.Key.ToString())
+                {
+                    case "DownArrow":
+                        choise++;
+                        break;
+                    case "UpArrow":
+                        choise--;
+                        break;
+                    case "Enter":
+                        runloop = false;
+                        break;
+                }
+
+                if (choise < 0) choise = list.Length - 1;
+                if (choise >= list.Length) choise = 0;
+            } while (runloop);
+
+            return choise;
+        }
+
+        private void PrintList(string[] list, int choiseIndex)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                string item = list[i];
+                if (i == choiseIndex) Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("> {0}", item);
+                Console.ResetColor();
+            }
+        }
+    }
+}
