e5e1f7d245cf52544e6fa4ebe8e0480477d0a3c3
Madu <kontakt.madu@gmail.com>
2024-02-21 17:38:05 +0100

    Added Printer

---
+++ Madu/Util/Printer.cs
@@ -0,0 +1,17 @@
+﻿/**
+* Printer
+* Copyright © Madu/Marvin - 2024
+*/
+namespace Madu.Util
+{
+    public class Printer
+    {
+        /// <summary>
+        /// Prints the separatorChar x-times (count) in one line
+        /// </summary>
+        public void PrintSeparator(char separatorChar = '-', int count = 50)
+        {
+            Console.WriteLine(new string(separatorChar, count));
+        }
+    }
+}
