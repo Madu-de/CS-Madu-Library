5149b716c36b50c9ac6d1e8c7e0e6419b9a78f38
Madu <kontakt.madu@gmail.com>
2024-02-21 17:05:38 +0100

    Added ArrayPrinter

---
+++ Madu/Arrays/ArrayPrinter.cs
@@ -0,0 +1,31 @@
+﻿/**
+* ArrayPrinter
+* Copyright © Madu/Marvin - 2024
+*/
+namespace Madu.Arrays
+{
+    /// <summary>
+    /// A class which includes methods to print an array
+    /// </summary>
+    public class ArrayPrinter
+    {
+        /// <summary>
+        /// Prints every element in its own line
+        /// </summary>
+        public void Print<T>(T[] arr)
+        {
+            foreach (T item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /// <summary>
+        /// Prints every element in one line
+        /// </summary>
+        public void OneLinePrint<T>(T[] arr, string separator = ", ")
+        {
+            Console.WriteLine(string.Join(separator, arr));
+        }
+    }
+}
